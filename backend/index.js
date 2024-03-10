9a490b88d7946d359e79a9d03bec4d5c792ee118
MarkJerielCabalbag <cabalbagmarkjeriel03@gmail.com>
2024-03-10 09:48:51 +0800

    first commit

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import mysql from "mysql2";
+
+const app = express();
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "testlang",
+});
+
+app.get("/", (req, res) => {
+  res.json("This is the backend ulet");
+});
+
+app.get("/test", (req, res) => {
+  const q = "SELECT * FROM testlang.admin";
+  db.query(q, (err, data) => {
+    if (err) return res.json("error message");
+    return res.json(data);
+  });
+});
+
+app.post("/test", (req, res) => {
+  const q = "INSERT INTO testlang.admin (`user`, `password`) VALUES (?)";
+  const values = ["index user", "index password"];
+  db.query(q, [values], (err, data) => {
+    if (err) return res.json("error message");
+    return res.json(data);
+  });
+});
+app.listen(8000, () => {
+  console.log("kamusta tulog lods?, from backend");
+});
