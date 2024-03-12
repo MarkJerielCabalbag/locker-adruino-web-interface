fb79e115c57df411f619b505db389b3fe12ac9eb
MarkJerielCabalbag <cabalbagmarkjeriel03@gmail.com>
2024-03-12 22:03:54 +0800

    fix layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,26 +3,29 @@ import React from "react";
 const App = () => {
   return (
     <div className="container-sm text-white">
-      <div className="d-flex flex-column">
+      <div
+        className="d-flex flex-column justify-content-center"
+        style={{ height: "100vh" }}
+      >
         <div className="d-flex" style={{ height: "200px" }}>
-          <div className="col border border-black h-75 bg-secondary d-flex align-items-center justify-content-center">
+          <div className="col border border-black h-100 bg-secondary d-flex align-items-center justify-content-center">
             Sanidad Building 1
           </div>
-          <div className="col border border-black h-75 bg-secondary d-flex align-items-center justify-content-center">
+          <div className="col border border-black h-100 bg-secondary d-flex align-items-center justify-content-center">
             Sanidad Building 2
           </div>
-          <div className="col border border-black h-75 bg-secondary d-flex align-items-center justify-content-center">
+          <div className="col border border-black h-100 bg-secondary d-flex align-items-center justify-content-center">
             Sanidad Building 3
           </div>
-          <div className="col border border-black h-75 bg-secondary d-flex align-items-center justify-content-center">
+          <div className="col border border-black h-100 bg-secondary d-flex align-items-center justify-content-center">
             Sanidad Building 4
           </div>
         </div>
-        <div className="d-flex" style={{ height: "100px" }}>
-          <div className="col border border h-25 bg-secondary border-black"></div>
-          <div className="col border border h-25 bg-secondary border-black"></div>
-          <div className="col border border h-25 bg-secondary border-black"></div>
-          <div className="col border border h-25 bg-secondary border-black"></div>
+        <div className="d-flex" style={{ height: "40px" }}>
+          <div className="col border border h-100 bg-secondary border-black"></div>
+          <div className="col border border h-100 bg-secondary border-black"></div>
+          <div className="col border border h-100 bg-secondary border-black"></div>
+          <div className="col border border h-100 bg-secondary border-black"></div>
         </div>
       </div>
     </div>
